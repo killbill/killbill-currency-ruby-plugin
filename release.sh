6a293ec12a47602f3d14fa45fd8b249a91b59b59
stephane brossier <stephane@ning.com>
2014-03-07 19:39:19 -0800

    Package renaming com.ning -> org.killbill

--- release.sh
+++ release.sh
@@ -31,7 +31,7 @@ rake killbill:package
 ARTIFACT="$PWD/pkg/killbill-currency-plugin-$VERSION.tar.gz"
 echo "Pushing $ARTIFACT to Maven Central"
 mvn gpg:sign-and-deploy-file \
-    -DgroupId=com.ning.killbill.ruby \
+    -DgroupId=org.kill-bill.ruby \
     -DartifactId=killbill-currency-plugin \
     -Dversion=$VERSION \
     -Dpackaging=tar.gz \
