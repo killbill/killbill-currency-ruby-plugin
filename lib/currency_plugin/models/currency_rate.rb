66820bc6a7cd7cafa2a2a37bf8c1f50b725a9520
Pierre-Alexandre Meyer <pmeyer@groupon.com>
2014-04-24 13:35:47 -0700

    gemspec: update to killbill 3.1.0
    Signed-off-by: Pierre-Alexandre Meyer <pmeyer@groupon.com>

--- lib/currency_plugin/models/currency_rate.rb
+++ lib/currency_plugin/models/currency_rate.rb
@@ -5,10 +5,6 @@ module Killbill
 
       belongs_to :currency_update
 
-      attr_accessible :target_currency,
-                      :rate,
-                      :currency_update_id
-
       scope :latest_rates_for_base_currency, ->(currency_update_id) { where("currency_update_id = ?", currency_update_id).order("target_currency ASC") }
     end
   end
--- lib/currency_plugin/models/currency_update.rb
+++ lib/currency_plugin/models/currency_update.rb
@@ -4,9 +4,6 @@ module Killbill
 
       has_many :currency_rates
 
-      attr_accessible :base_currency,
-                      :conversion_date
-
       scope :historical_base_currencies, ->(base_currency_arg) { where("base_currency = ?", base_currency_arg).order("conversion_date DESC") }
       scope :latest_base_currency, ->(base_currency_arg) { historical_base_currencies(base_currency_arg).limit(1) }
       scope :distinct_base_currencies, -> { select("DISTINCT base_currency").order("base_currency ASC") }
